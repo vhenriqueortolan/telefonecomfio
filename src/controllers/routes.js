9b3553cba48d52835b66ffa08e30ec524784584f
Victor Henrique Ortolan <87250743+vhenriqueortolan@users.noreply.github.com>
2021-12-29 16:13:40 -0300

    include new routes

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const app = express()
-const path = require('path')
 const router = express.Router()
 
 router.get('/', function(req, res) {
     res.render('pages/inicio')
 })
 
-router.get('/sobre', function(req,res) {
+router.get('/sobre', function(req, res) {
     res.render('pages/sobre')
+})
+
+router.get('/como-ajudar', function(req, res){
+    res.render('pages/como-ajudar')
+})
+
+router.get('/contato', function(req, res){
+    res.render('pages/contato')
 })
 
 module.exports = router
